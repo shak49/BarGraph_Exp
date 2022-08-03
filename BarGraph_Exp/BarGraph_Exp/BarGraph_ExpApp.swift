911a2a0cac23714d21b492171b52be87ba7cfe7a
Shak49 <shakfeizi@gmali.com>
2022-08-03 13:00:13 -0500

    project's files added

---
+++ BarGraph_Exp/BarGraph_Exp/BarGraph_ExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarGraph_ExpApp.swift
+//  BarGraph_Exp
+//
+//  Created by Shak Feizi on 8/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BarGraph_ExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
